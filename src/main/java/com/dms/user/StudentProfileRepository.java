7bfef2afc01f7bf904ff4ce601e706dc4e50a1fb
ppand <ppandey768500@gmail.com>
2026-08-19 00:48:46 +0530

    feat(user): look up student profiles by programme

--- src/main/java/com/dms/user/StudentProfileRepository.java
+++ src/main/java/com/dms/user/StudentProfileRepository.java
@@ -1,11 +1,16 @@
 package com.dms.user;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
     Optional<StudentProfile> findByUser(User user);
     Optional<StudentProfile> findByUserEmail(String email);
     Optional<StudentProfile> findByRollNo(String rollNo);
+
+    @EntityGraph(attributePaths = "user")
+    List<StudentProfile> findByProgrammeOrderByRollNoAsc(Programme programme);
 }
